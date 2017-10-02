0157725afb7d8d3de789f52444b630adfbff8eba
iandioch <iandioch11@gmail.com>
2017-10-02 14:50:44 -0400

    kanye is too slow

--- steely/plugins/kanye.py
+++ steely/plugins/kanye.py
@@ -3,28 +3,40 @@
 Reimagine the last message as if it were said by Snoop Dogg himself.
 """
 
+import difflib
 import json
 import requests
-from bs4 import BeautifulSoup
-
+from whoosh.spelling import ListCorrector
 
 __author__ = 'iandioch'
 COMMAND = '.kanye'
-KANYEREST_URL = 'http://kanyerest.xyz/api/album/'
-ALBUMS = ['graduation']
-WORDS = set()
+KANYEREST_URL = 'http://kanyerest.xyz/api/counter'
+WORDS = None
 
 def load():
-	for album in ALBUMS:
-		url = KANYEREST_URL + album
-		r = requests.post(url)
-		data = json.loads(r.text)
-		for song in data['result']:
-			WORDS = WORDS.union(song['lyrics'])
-	print(WORDS)
+    r = requests.get(KANYEREST_URL)
+    data = json.loads(r.text)
+    words = sorted(data, key = lambda x: data[x], reverse=True)
+    print('Kanye dict is {} words', len(words))
+    print(words[:50])
+    return (words)
 
 def kanye(text):
-	return text
+    words = text.split()
+    if len(words) > 100:
+        return 'My greatest pain in life is that I will never be able to see myself perform live.'
+    out = []
+    for word in words:
+        if False or word in WORDS:
+            out.append(word)
+            continue
+        # best_guesses = difflib.get_close_matches(word, WORDS, 1, 0)
+        best_guesses = corrector.suggest(word, limit=1, maxdist=5)
+        if len(best_guesses) > 0:
+            out.append(best_guesses[0])
+        else:
+            out.append(word)
+    return ' '.join(out)
 
 
 def main(bot, author_id, message, thread_id, thread_type, **kwargs):
@@ -34,4 +46,5 @@ def main(bot, author_id, message, thread_id, thread_type, **kwargs):
                     thread_type=thread_type)
 
 # To be run at boot
-load()
+WORDS = load()
+corrector = ListCorrector(WORDS)
